dd83a27318d69b804fa121ab04e24a5c98009071
Vishal Gupta <guptavishal61380@gmail.com>
2024-01-02 16:09:59 +0530

    first commit

---
+++ lib/Screens/home_screen.dart
@@ -0,0 +1,66 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:restapi/Model/post.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late Future<List<Post>> result;
+
+  @override
+  void initState() {
+    super.initState();
+    result = fetchData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('REST API'),
+        centerTitle: true,
+      ),
+      body: FutureBuilder(
+        future: result,
+        builder: (context, AsyncSnapshot<List<Post>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          } else if (snapshot.hasError) {
+            return Text('ERROR ===> ${snapshot.error}');
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Text('No data available');
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  //title: Text(snapshot.data![index].title ?? 'No Title'),
+                  title: Text(snapshot.data![index].title!,style: const TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+                  subtitle: Text(snapshot.data![index].body!),
+                );
+              },
+            );
+          }
+        },
+      ),
+    );
+  }
+
+  Future<List<Post>> fetchData() async {
+    final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body);
+      final List<Post> posts = data.map((json) => Post.fromJson(json)).toList();
+      return posts;
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+}
